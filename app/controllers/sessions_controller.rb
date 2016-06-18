b28307f9d8e69f571eb9a5cddbf6b46c8a82eaa4
Stuart Kam <stuart.kam17@gmail.com>
2016-06-17 16:01:58 -1000

    login/logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+      #error message  
+  def new
+  end
+
+  def create
+  	user = User.find_by(email: params[:session][:email])
+  	if user && user.authenticate(params[:session][:password])
+  		#log user in
+  		log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  		redirect_to user
+  	else
+	  	#error message	
+	  	flash.now[:danger] = "Invalid password combination!"
+	  	render 'new'
+  	end
+ 	end
+
+ 	def destroy
+    log_out if logged_in?
+    redirect_to root_url
+ 	end
+
+end
